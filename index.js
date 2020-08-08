57c61fd6719554f6c6f3dfe35fcbe32b1934c1ab
Mark Franciose <mark.franciose@gmail.com>
2020-08-08 16:40:28 -0500

    print help message

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('this is the entrypoint, hi');
+'use strict'
+
+var argv = require('minimist')(process.argv.slice(2));
+var baseOptions = argv._
+var firstOption = baseOptions[0];
+
+var { mainHelp } = require('./lib/help');
+var options = {
+  'help': mainHelp,
+};
+
+/* basic help printing */
+if (argv.h || argv.help) mainHelp();
+if ( Object.keys(options).includes(firstOption) ) {
+  options[firstOption]();
+}
